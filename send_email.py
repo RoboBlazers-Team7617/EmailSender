b15cf8bfd7670cc5f7ab63a89df4870d0b783bd9
RoboBlazers Team #7617 <46693100+RoboBlazers-Team7617@users.noreply.github.com>
2019-08-24 21:06:35 -0400

    Add files via upload

--- send_email.py
+++ send_email.py
@@ -1,18 +1,28 @@
 # Initialization
-MY_ADDRESS = "youremail"
-PASSWORD = "yourpassword"
+MY_ADDRESS = input("Enter your email address: ")
+PASSWORD = input("Enter your password: ")
+NAME = input("Enter your name: ")
+
+# Read in the file
+with open('message.txt', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('[name]', NAME)
+
+# Write the file out again
+with open('message.txt', 'w') as file:
+  file.write(filedata)
 
 # Function to read the contacts from contacts.txt and return a
 # list of names and email addresses
 def get_contacts(filename):
-    names = []
     emails = []
     with open(filename, mode='r', encoding='utf-8') as contacts_file:
         for a_contact in contacts_file:
-            names.append(a_contact.split()[0])
-            emails.append(a_contact.split()[1])
+            emails.append(a_contact.split()[0])
     #returns two seperate arrays with all the names and emails
-    return names, emails
+    return emails
 
 # We also need a function to read in a template file (like message.txt)
 # and return a Template object made from its contents
@@ -32,20 +42,20 @@ s = smtplib.SMTP(host='smtp.gmail.com', port=587)
 s.starttls()
 s.login(MY_ADDRESS, PASSWORD)
 
-names, emails = get_contacts('contacts.txt')  # read contacts
+emails = get_contacts('contacts.txt')  # read contacts
 message_template = read_template('message.txt')
 
 # Actually send email
 # import necessary packages
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from email.mime.application import MIMEApplication
 # For each contact, send the email:
-for name, email in zip(names, emails):
+for email in emails:
     msg = MIMEMultipart()       # create a message
 
     # add in the actual person name to the message template
-    message = message_template.substitute(PERSON_NAME=name.title())
+    message = message_template.substitute()
 
     # setup the parameters of the message
     msg['From']=MY_ADDRESS
@@ -54,6 +64,10 @@ for name, email in zip(names, emails):
 
     # add in the message body
     msg.attach(MIMEText(message, 'plain'))
+
+    pdfpart = MIMEApplication(open('Second Year Fundraising Plan.pdf', 'rb').read())
+    pdfpart.add_header('Content-Disposition', 'attachment', filename='Second Year Fundraising Plan.pdf')
+    msg.attach(pdfpart)
 
     # send the message via the server set up earlier.
     s.send_message(msg)
